7c1107d9f988da0ae6062c63ddb0c950861530a8
Davide Dal Pos <daveliga@gmail.com>
2024-05-10 13:14:09 -0400

    HAO

--- phenospy_package/phenospy/make_snippets.py
+++ phenospy_package/phenospy/make_snippets.py
@@ -1,4 +1,4 @@
 from phenospy import *
 
-yaml_file = '"E:\Phenoscript\PhenoScript_Ichneumonidae\phenospy_package\phenospy\make_snippets.py"'
+yaml_file = 'Phenoscript\PhenoScript_Ichneumonidae\phenospy_package\phenospy\make_snippets.py'
 make_vscodeSnips(yaml_file)
